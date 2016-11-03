6ae81049872eeea022e5099fa004f10b126d64cb
152772a <152772a@mymail.nyp.edu.sg>
2016-11-03 14:59:26 +0800

    Initial commit

---
+++ app/src/main/java/com/nyp/sit/dit/it2107/mylinearlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nyp.sit.dit.it2107.mylinearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
